fb2ee63052d171cb2d38a135e6dc5641eb0b54bc
Eduardo Serete <eduardo.serete@oeconnection.com>
2026-03-16 20:41:34 +0000

    feat: hq schema command

--- cmd/hq/commands/schema.go
+++ cmd/hq/commands/schema.go
@@ -1,7 +1,68 @@
+// cmd/hq/commands/schema.go
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/eduardoserete/humanized-query/internal/config"
+	"github.com/eduardoserete/humanized-query/internal/executor"
+	"github.com/eduardoserete/humanized-query/internal/schema"
+	"github.com/spf13/cobra"
+)
 
 func schemaCmd() *cobra.Command {
-	return &cobra.Command{Use: "schema", Short: "Introspect database schema"}
+	var dbName, tableFilter string
+
+	cmd := &cobra.Command{
+		Use:   "schema",
+		Short: "Introspect database schema and return JSON",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfgPath, err := configPath()
+			if err != nil {
+				return writeError("config_error", err.Error())
+			}
+			cfg, err := config.Load(cfgPath)
+			if err != nil {
+				return writeError("config_error", err.Error())
+			}
+
+			dbCfg, err := cfg.DB(dbName)
+			if err != nil {
+				return writeError("db_not_found", fmt.Sprintf("no database configured with name %q", dbName))
+			}
+
+			adapter, err := executor.New(dbCfg.Dialect)
+			if err != nil {
+				return writeError("unsupported_dialect", err.Error())
+			}
+
+			db, err := adapter.Open(dbCfg.DSN)
+			if err != nil {
+				return writeError("connection_error", err.Error())
+			}
+			defer db.Close()
+
+			introspector, err := schema.New(dbCfg.Dialect)
+			if err != nil {
+				return writeError("unsupported_dialect", err.Error())
+			}
+
+			s, err := introspector.Introspect(db, dbName, tableFilter)
+			if err != nil {
+				return writeError("introspection_error", err.Error())
+			}
+
+			enc := json.NewEncoder(os.Stdout)
+			enc.SetIndent("", "  ")
+			return enc.Encode(s)
+		},
+	}
+
+	cmd.Flags().StringVar(&dbName, "db", "", "Database name (required)")
+	cmd.Flags().StringVar(&tableFilter, "table", "", "Filter to a specific table")
+	cmd.MarkFlagRequired("db")
+
+	return cmd
 }
